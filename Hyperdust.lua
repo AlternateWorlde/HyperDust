f66b2d845241c5081b7f0ed39229e9be74ca7a32
AlternateWorlde <94998907+AlternateWorlde@users.noreply.github.com>
2021-11-24 22:11:16 +0000

    Add files via upload

---
+++ Hyperdust.lua
@@ -0,0 +1,500 @@
+--// Hyperdust Made By SussyImposterYes#2845 Keybinds: 1 2 3 4 5
+game:GetService("Players").LocalPlayer.PlayerGui.Menu.Characs.FatalE.Visible = true
+wait(3)
+game.Players.LocalPlayer.PlayerGui.Musics.FatalError.TimePosition = "0"
+wait(2)
+--PlaceHolder SongID 5722886482
+game.Players.LocalPlayer.PlayerGui.Musics.FatalError.SoundId = "rbxassetid://6275778499"
+local Player = game.Players.localPlayer
+local char = Player.Character
+local Character = Player.Character
+
+local Event = game:GetService("ReplicatedStorage").InfoChara
+Event:FireServer()
+wait(1)
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local Event = game:GetService("ReplicatedStorage").InfoXChara
+Event:FireServer()
+
+local player = game.Players.LocalPlayer
+local mouse = player:GetMouse()
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Made By King Dream#9746";
+    Text = "Ink Sans Loaded Enjoy!";
+    Duration = 8;
+})
+
+local A_1 = "[???] ..."
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "[???] Heh..."
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "[???] HAHAHAHAHA"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1.5)
+local A_1 = "[???] BUDDY..."
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "[Hyper Dust Sans] LETS HAVE A MAD TIME"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+
+
+
+
+
+mouse.KeyDown:connect(function(key) 
+    if key == "1" then
+local A_1 = "[HYPER DUST SANS] DIE KID"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").Sans1GasterBlaster
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans2
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans3
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans4
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans2BTGasterBlaster
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans3BT
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Sans4BT
+Event:FireServer()
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "2" then
+local A_1 = "[HYPER DUST SANS] WELCOME TO MY SPECIAL HELL"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").Dio4Phase2
+Event:FireServer()
+wait(5)
+local Event = game:GetService("ReplicatedStorage").SpinelAttack2
+Event:FireServer()
+
+wait(1)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+
+
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "3" then
+local A_1 = "[HYPER DUST SANS] YOU WILL DIE TO ME"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+wait(2)
+local Event = game:GetService("ReplicatedStorage").CharaReviveAttack
+Event:FireServer()
+
+
+
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "4" then
+local A_1 = "[HYPER DUST SANS] UNIVERSE DESTROY"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").Luffy4
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").Luffy4
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").Luffy4
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").Luffy4
+Event:FireServer()
+
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "5" then
+local A_1 = "[HYPER DUST SANS] JUST DIE ALREADY"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2) 
+local Event = game:GetService("ReplicatedStorage").SSKnifes3
+Event:FireServer()
+wait(1.5)
+
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage").Flowey0
+Event:FireServer()
+
+
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "6" then
+local A_1 = "[HYPER DUST SANS] LETS SEE HOW YOU SURVIVE THIS"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+wait(0.5)
+local Event = game:GetService("ReplicatedStorage").OrangeSlash
+Event:FireServer()
+ 
+end
+end)
+
+mouse.KeyDown:connect(function(key) 
+    if key == "," then
+local A_1 = "[HYPER DUST SANS] ..."
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(1)
+local A_1 = "[HYPER DUST SANS] All the exp i gained..."
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait(2)
+local A_1 = "[HYPER DUST SANS] CANT GO TO WASTE"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+local Event = game:GetService("ReplicatedStorage")["XChara_Attack1Button"]
+Event:FireServer()
+
+
+
+end
+end)
